4e4ad18f240993cc588ffb859ed895f403cf7171
simpossible <ljfsteven@163.com>
2017-08-11 16:45:57 +0800

    firstversion

---
+++ IBLPlayer/IBLPlayer/IBLPlayer.h
@@ -0,0 +1,13 @@
+//
+//  IBLPlayer.h
+//  IBLPlayer
+//
+//  Created by simpossible on 11/08/2017.
+//  Copyright © 2017 Ahead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBLPlayer : NSObject
+
+@end
---
+++ IBLPlayer/IBLPlayer/IBLPlayer.m
@@ -0,0 +1,13 @@
+//
+//  IBLPlayer.m
+//  IBLPlayer
+//
+//  Created by simpossible on 11/08/2017.
+//  Copyright © 2017 Ahead. All rights reserved.
+//
+
+#import "IBLPlayer.h"
+
+@implementation IBLPlayer
+
+@end
